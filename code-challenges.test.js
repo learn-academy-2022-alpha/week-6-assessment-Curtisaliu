8eb2baf861bc9637a729d875aba05fd30b19b402
Curtis A. Liu <Curtisaliu@gmail.com>
2022-04-02 22:47:53 -0700

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,38 +14,129 @@
 
 // a) Create a test with an expect statement using the variable provided.
 
-const people = [
-  { name: "ford prefect", occupation: "a hitchhiker" },
-  { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
-  { name: "arthur dent", occupation: "a radio employee" }
-]
-// Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+describe("result", () => {
+  it("takes in an array of objects and returns an array with a sentence about each person with their name capitalized", () => {
+
+    const people = [
+      { name: "ford prefect", occupation: "a hitchhiker" },
+      { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
+      { name: "arthur dent", occupation: "a radio employee" }
+    ]
+    // Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+
+    expect(result(people)).toEqual(["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."])
+  })
+})
+
+// result › takes in an array of objects and returns an array with a sentence about each person with their name capitalized
+// ReferenceError: result is not defined
+// Got Red. 
 
 
 // b) Create the function that makes the test pass.
+// Create a function named result that will pass a string in the parameter
+// likely will need to iterate through the object. .map/.filter for loop?
+// store a variable that contains the built-in methods for capitalization 
+// string interpolation. 
+
+
+const result = (string) => {
+  return string.map(value => {
+    let cap = value.name.split(" ").map(x => x.charAt(0).toUpperCase() + x.slice(1)).join(" ")
+    return `${cap} is ${value.occupation}.`
+  })
+}
+
+// Got Green. 
+
 
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
 // a) Create a test with an expect statement using the variables provided.
 
-const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
-// Expected output: [ 2, 0, -1, 0 ]
-const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
-// Expected output: [ 2, 1, -1 ]
+describe("rem3", () => {
+  it("takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3", () => {
+
+    const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
+    // Expected output: [ 2, 0, -1, 0 ]
+    const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
+    // Expected output: [ 2, 1, -1 ]
+
+    expect(rem3(hodgepodge1)).toEqual([2, 0, -1, 0])
+    expect(rem3(hodgepodge2)).toEqual([2, 1, -1])
+  })
+})
+
+// rem3 › takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3
+// ReferenceError: rem3 is not defined
+// Got Red.
 
 
 // b) Create the function that makes the test pass.
+// Create a function named rem3 that takes in an array in its parameters
+// Make an empty array 
+// Will need to iterate through the array to find integers --> .filter? forloop?
+// iteration must disregard anything that is not an integer --> use typeof method?
+// Will have a modulo 3 somewhere
+// Push into the empty array
+
+
+const rem3 = (array) => {
+  let newArr = []
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] === 'number') {
+      newArr.push(array[i] % 3)
+    }
+  }
+  return newArr
+}
+
+// Got Green. 
+
+
+
 
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
 // a) Create a test with an expect statement using the variables provided.
 
-const cubeAndSum1 = [2, 3, 4]
-// Expected output: 99
-const cubeAndSum2 = [0, 5, 10]
-// Expected output: 1125
+describe("sumCubed", () => {
+  it("takes in an array of numbers and returns the sum of all the numbers cubed.", () => {
 
+    const cubeAndSum1 = [2, 3, 4]
+    // Expected output: 99
+    const cubeAndSum2 = [0, 5, 10]
+    // Expected output: 1125
+
+    expect(sumCubed(cubeAndSum1)).toEqual(99)
+    expect(sumCubed(cubeAndSum2)).toEqual(1125)
+  })
+})
+
+// ● sumCubed › takes in an array of numbers and returns the sum of all the numbers cubed.
+// ReferenceError: sumCubed is not defined
+// Got Red. 
 
 // b) Create the function that makes the test pass.
+// Create a function that will pass in an array as its parameters
+// likely will need an empty array 
+// likely need a variable that equals to zero 
+// Must iterate through the array --> .map? for loop?
+// Find a way to cube all the elements in the array individually. 
+// add the elements together after they are cubed. 
+
+
+const sumCubed = (array) => {
+  let x = []
+  let sum = 0
+  for (let i = 0; i < array.length; i++) {
+    array[i] = array[i] * array[i] * array[i]
+    sum += array[i]
+  }
+  return sum
+}
+
+// Got Green
+
--- rails-commenting.rb
+++ rails-commenting.rb
@@ -7,24 +7,33 @@
 # FILE: app/controller/blog_posts_controller.rb
 
 # ---1)
+# I believe this is the model class which is automatically generated  when we generate a model and run a database migration. 
+# def index is a controller method that will allow access to all the items from the database. 
 class BlogPostsController < ApplicationController
   def index
     # ---2)
+    # @posts is an instance variable which will save all the content from the database as an active record call.
+    # This is denoted by the .all at the end. 
     @posts = BlogPost.all
   end
 
   # ---3)
+  # Define show is declaring a method. Show is a RESTFul route method that looks for one item in the database. 
+  # Post is an instance variable that is an active record to find one item by the id. The id will coem from the url params. 
   def show
     @post = BlogPost.find(params[:id])
   end
 
   # ---4)
+  # New is a RESTful route method that displays a form for the user, thus allowing users to add information to the web application to store in the database.
   def new
     @post = BlogPost.new
   end
 
   def create
     # ---5)
+    # @post is an instance variable for the method create which is a restful route that submits user data to the database. 
+    # Underneath the instance variable, is a conditional which redirects the user if they enter information that submits correctly to the blog post that they entered. If the user enter something that was not register correctly for the blog post they will be redirected to the page in which they can create a new blog post again. 
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -35,12 +44,19 @@ class BlogPostsController < ApplicationController
 
   def edit
     # ---6)
+    # Define edit is creating a method in order to create an edit action that will handle the rednering of the edit form. This particular form can be find typing in the id. Additionally, the particular form can be updated by being found using the id.
     @post = BlogPost.find(params[:id])
   end
 
   def update
     @post = BlogPost.find(params[:id])
     # ---7)
+    # The update method is an action that will actually update the database record itself.
+    # If the update was successful, the user will be redirected to the blog post that they updated. 
+    # If the update was not successful, the user will be redirected to the same page in which they can try to update again. 
+    # The destroy method removes the record from the database. 
+    # The conditional will either redirect the user's browser to the langing page or will redirect them to the page in which they can try to destroy the post again. 
+
     @post.update(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -55,14 +71,20 @@ class BlogPostsController < ApplicationController
       redirect_to blog_posts_path
     else
       # ---8)
+      # An else conditional in which if the destroy action fails, the user will be redirected to the same page. 
+
       redirect_to blog_post_path(@post)
     end
   end
 
   # ---9)
+  # By using private, the method is only usable in the class. Thus, the method cannnot be called to another class. 
+  # def blog_post_params is just declaring the method. 
+
   private
   def blog_post_params
     # ---10)
+    # The user will only be able to pass in the params of title and content. 
     params.require(:blog_post).permit(:title, :content)
   end
 end
